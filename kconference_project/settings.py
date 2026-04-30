23033e751e8cb731464426ef37cb062da2f92e64
y5c99 <y5c99@students.keele.ac.uk>
2026-04-30 17:29:53 +0100

    added password validation

--- kconference_project/settings.py
+++ kconference_project/settings.py
@@ -92,15 +92,15 @@ DATABASES = {
 
 # ---- Password validation ----
 # Django checks that passwords aren't too weak.
-AUTH_PASSWORD_VALIDATORS = [
-]
-
-#AUTH_PASSWORD_VALIDATORS = [
-  #  {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
-    #{'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator'},
-    #{'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator'},
-    #{'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator'},
+##AUTH_PASSWORD_VALIDATORS = [
 #]
+
+AUTH_PASSWORD_VALIDATORS = [
+   {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator'},
+]
 
 
 # ---- Internationalisation ----
